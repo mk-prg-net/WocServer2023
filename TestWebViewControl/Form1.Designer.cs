50a4722cba1e185f63f86fd8e59206ff40bb74c2
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2021-05-10 23:39:19 +0200

    Projekt TestWebViewControl hinzugefügt, mit dem derEdge- Browser aus der Winforms gestartet werden kann.

---
+++ TestWebViewControl/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace TestWebViewControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView1 = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView1
+            // 
+            this.webView1.Location = new System.Drawing.Point(0, 81);
+            this.webView1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webView1.Name = "webView1";
+            this.webView1.Size = new System.Drawing.Size(800, 369);
+            this.webView1.Source = new System.Uri("http://mk-prg-net.de", System.UriKind.Absolute);
+            this.webView1.TabIndex = 0;
+            this.webView1.DOMContentLoaded += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs>(this.webView1_DOMContentLoaded);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 75);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Toolkit.Forms.UI.Controls.WebView webView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestWebViewControl/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DT = MKPRG.Tracing.DocuTerms;
+
+namespace TestWebViewControl
+{
+    public partial class Form1 : Form
+    {
+        DT.IComposer pnL = new DT.Composer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webView1_DOMContentLoaded(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs e)
+        {
+            MessageBox.Show("Inhalt geladen");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var htm = new MKPRG.HTML.HTMLDocument(pnL);
+
+            htm.h1.txt("Hallo Welt ").html(MKPRG.Naming.Glyphs.Geographic.Globe).E
+                .p.txt("Meine erste Testseite").E
+                .build();
+
+            webView1.NavigateToString(htm.CloseDoc());
+        }
+    }
+}
