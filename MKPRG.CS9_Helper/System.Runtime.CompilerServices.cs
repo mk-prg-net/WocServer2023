b18403e203beb4dc1d62f9fe5095534a24e5309f
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2022-05-01 17:45:45 +0200

    CS9 Helper Projekt verfeinert

--- MKPRG.CS9_Helper/System.Runtime.CompilerServices.cs
+++ MKPRG.CS9_Helper/System.Runtime.CompilerServices.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 /// <summary>
 /// mko, 1.5.2022
 /// Ermöglicht die Kompilation von C# 9 Code in "alten" .NET Framework Projekten
-/// Siehe auch https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
+/// Siehe auch 
+/// 1. [C#9](https://sergiopedri.medium.com/enabling-and-using-c-9-features-on-older-and-unsupported-runtimes-ce384d8debb)
+/// 2. [bugFix](https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined)
 /// 
 /// </summary>
 namespace System.Runtime.CompilerServices
