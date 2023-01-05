4f74ef375a63719e8bf5556d8029f4684411ba2b
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2023-01-05 23:04:21 +0100

    Typescript in Article Edit installiert

---
+++ ArticleEdit/Scripts/ts/myFirst.ts
@@ -0,0 +1,18 @@
+﻿// mko, 5.1.2023
+// Testscript
+
+import $ from "jquery-1.10.2.min";
+
+function greeting(txt: string, time: string): string {
+    return `${time} ${txt}`;
+}
+
+
+
+
+
+
+var g = greeting("Hallo Welt", "9:00");
+
+let A = 99;
+
---
+++ ArticleEdit/Scripts/tsJs/myFirst.js
@@ -0,0 +1,8 @@
+// mko, 5.1.2023
+// Testscript
+function greeting(txt, time) {
+    return `${time} ${txt}`;
+}
+var g = greeting("Hallo Welt", "9:00");
+let A = 99;
+//# sourceMappingURL=myFirst.js.map
